dcf2ce8b5e486d774f813db6ddb109fe32583cd6
Benjamin <benjamin.villarroel3@gmail.com>
2023-10-03 11:02:47 -0300

    Esqueleto del programa

--- tarea1.c
+++ tarea1.c
@@ -5,9 +5,55 @@ int isZero(int x) {
 	return !x;
 }
 
+// Problema 2
+int minusOne(void) {
+	return 0;
+}
+
+// Problema 3
+int bitNor(int x, int y) {
+	return 0;
+}
+
+// Problema 4
+int getByte(int x, int n) {
+	return 0;
+}
+
+// Problema 5
+int isNotEqual(int x, int y) {
+	return 0;
+}
+
+// Problema 6
+int negate(int x) {
+	return 0;
+}
+
+// Problema 7
+int isPositive(int x) {
+	return 0;
+}
+
+// Problema 8
+int isGreater(int x, int y) {
+	return 0;
+}
+
+// Problema 9
+int reverseBytes(int x) {
+	return 0;
+}
+
+// Problema 10
+int isPower2(int x) {
+	return 0;
+}
+
 int main() {
 	int x = 0;
 	printf("%d\n", isZero(x));
 	x = 3;
 	printf("%d\n", isZero(x));
+	printf("%d\n",x);
 }
