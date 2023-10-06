51861d38f30ee609c0828e95bce0019b3a361bad
Juan Agustin <60883242+Juan-Agustin-gits@users.noreply.github.com>
2023-10-06 00:03:15 -0300

    solo faltan 3 funciones tarea1.c

--- tarea1.c
+++ tarea1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
 // Problema 1
-int isZero(int x) {
-	/* Si la variable es nula, retorna 1. De lo contrario, retorna 0 */
+int isZero(int x) {/* Si la variable es nula, retorna 1. De lo contrario, retorna 0 (gcc (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0)*/
 	return !x;
 }
 
 // Problema 2
-int minusOne(void) {
-	return 0;
+int minusOne(void) {// retorna un -1 (gcc (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0)
+	return -1;
 }
 
-// Problema 3
-int bitNor(int x, int y) {
-	return 0;
+//Problema 3
+int bitNor(int x, int y) {// hace ~(x|y) pero sin usar el OR | (gcc (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0)
+	return ~x & ~y;
 }
 
 // Problema 4
-int getByte(int x, int n) {
-	return 0;
+int getByte(int x, int n) {// extrane el byte n de un word (gcc (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0)
+	return (!((x>>30)>>1));
 }
 
 // Problema 5
@@ -26,13 +26,13 @@ int isNotEqual(int x, int y) {
 }
 
 // Problema 6
-int negate(int x) {
-	return 0;
+int negate(int x) {// retorna el negativo de n (gcc (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0)
+	return ~x+1;
 }
 
 // Problema 7
-int isPositive(int x) {
-	return 0;
+int isPositive(int x) {//Devuelve 1 si x>0 y 0 si no lo es (Devuelve 1 si x>0 y 0 en otro caso.)
+	return ( ! ( ( x >> 30 ) >> 1 ) );
 }
 
 // Problema 8
@@ -51,9 +51,14 @@ int isPower2(int x) {
 }
 
 int main() {
-	int x = 0;
-	printf("%d\n", isZero(x));
-	x = 3;
-	printf("%d\n", isZero(x));
-	printf("%d\n",x);
+	/*int x;
+		scanf("%d", &x);
+		printf("%d \n", isZero(x));*/
+
+	//printf("%d \n", minusOne());
+
+	//printf("%d\n", bitNor(4,5));
+
+	printf("%d \n", getByte(0x12345678, 1) );
+	return 0;
 }
